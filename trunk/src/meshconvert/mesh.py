28f2edf755d5f39ef8b1d97879ca0b7be00f53a0
bouzim <bouzim@1292ab78-9140-0410-bef9-b961eb31b77f>
2007-12-12 21:56:43 +0000

    * src/meshconvert/mesh.py: Add a simple reader, it only accepts triangles.

--- trunk/src/meshconvert/mesh.py
+++ trunk/src/meshconvert/mesh.py
@@ -3,10 +3,50 @@
 # Mesh file format for medit.  See file format description (French only) in
 #  http://www.ann.jussieu.fr/~frey/logiciels/Docmedit.dir/Fichiers/formatmesh.pdf
 
-import string, glob, fileinput, posix
+import glob, fileinput, posix
+import logging
+import generic
 
-def reader(file):
-	raise ValueError, "Module "+`__name__`+" does not define a reader method"
+class reader(generic.reader):
+	indexed = True
+
+	def _section(self, label):
+		"Finds a labeled section in file"
+		if self.logger.isEnabledFor(logging.DEBUG):
+			self.logger.debug("Searching for section labeled '%s'" % label)
+		while True:
+			line = self.getline()
+			if (line == label):
+				return
+
+	def readNode(self):
+		"Gets next node"
+		self._section("Vertices")
+		number = int(self.getline())
+		if self.logger.isEnabledFor(logging.DEBUG):
+			self.logger.debug("Mesh contains %d vertices" % number)
+		for i in xrange(number):
+			line = self.getline()
+			coord = line.split()
+			yield generic.node(coord[0], coord[1], coord[2], label=str(i+1), color=coord[3])
+
+	def readElementIndexed(self):
+		"Gets next element."
+		try:
+			self._section("Triangles")
+		except IOError:
+			# EOF reached, reset file
+			self.f.seek(0)
+			self._section("Triangles")
+
+		number = int(self.getline())
+		if self.logger.isEnabledFor(logging.DEBUG):
+			self.logger.debug("Mesh contains %d triangles" % number)
+		for i in xrange(number):
+			line = self.getline()
+			nodeList = line.split()
+			last = nodeList.pop()
+			yield generic.indexedElement("Tri3", nodeList, label=str(i+1), color=last)
 
 def writer(file, reader):
 	if not reader.indexed:
@@ -52,7 +92,7 @@ Vertices
 		while True:
 			e = elements.next()
 			assert e.type == "Tri3"
-			f.write(string.join([nodeIndices[i] for i in e.list])+" "+e.color+"\n")
+			f.write(" ".join([nodeIndices[i] for i in e.list])+" "+e.color+"\n")
 			elementCounter += 1
 	except StopIteration:
 		pass
@@ -61,6 +101,8 @@ Vertices
 SubDomainFromMesh
 1
 3 1 1 1
+
+End
 """)
 	f.close()
 
