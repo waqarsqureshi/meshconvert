3e4e04b735f034adf544f2f106125d30542d8dbc
bouzim <bouzim@1292ab78-9140-0410-bef9-b961eb31b77f>
2007-12-13 21:43:59 +0000

    * src/meshconvert/tests: Initial support for unit tests.

---
+++ trunk/src/meshconvert/tests/test_unv_unv.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+
+import unittest
+import os, sys
+import StringIO
+
+dirname = os.path.dirname(__file__)
+if (dirname == ''):
+	dirname = '.'
+dirname = os.path.realpath(dirname)
+parent = os.path.split(dirname)[0]
+if parent not in sys.path:
+	sys.path.append(parent)
+
+import unv
+
+class unvTest(unittest.TestCase):
+	strTest1 = """    -1
+  2411
+         1         1         1         1
+   0.0000000000000000D+00   0.0000000000000000D+00   0.0000000000000000D+00
+         2         1         1         1
+   0.0000000000000000D+00   0.0000000000000000D+00   0.0000000000000000D+00
+         3         1         1         1
+   0.0000000000000000D+00   0.0000000000000000D+00   0.0000000000000000D+00
+    -1
+    -1
+  2412
+         1        91         1         1         1         3
+         1         2         3
+    -1
+"""
+	def test1(self):
+		reader = unv.reader(StringIO.StringIO(self.strTest1), string=True)
+		outputString = StringIO.StringIO()
+		unv.writer(outputString, reader, string=True)
+		res = outputString.getvalue()
+		outputString.close()
+		self.assertEquals(res, self.strTest1)
+
+def test_suite():
+	tests = [unittest.makeSuite(unvTest)]
+	return unittest.TestSuite(tests)
+
+if __name__ == '__main__':
+	unittest.main(defaultTest='test_suite')
+
