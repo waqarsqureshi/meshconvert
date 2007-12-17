6bd230b019e634cdb9c948d74810b655b24c1dd4
bouzim <bouzim@1292ab78-9140-0410-bef9-b961eb31b77f>
2007-12-17 20:44:48 +0000

    * src/meshconvert/off.py: Implement writer.

--- trunk/src/meshconvert/off.py
+++ trunk/src/meshconvert/off.py
@@ -2,9 +2,59 @@
 
 # OFF file format  http://shape.cs.princeton.edu/benchmark/documentation/off_format.html
 
+import tempfile
+
 def reader(file):
 	raise ValueError, "Module "+`__name__`+" does not define a reader method"
 
-def writer(file, reader):
-	raise ValueError, "Module "+`__name__`+" does not define a writer method"
+def writer(file, reader, string=False):
+	"Reads mesh from a reader and write it into a OFF file"
+	if not reader.indexed:
+		raise RuntimeError, "Cannot convert from non-indexed format to indexed format"
+	if string:
+		out = file
+		file = ""
+	else:
+		out = open(file, "w")
+
+	tempFiles = [tempfile.TemporaryFile(mode='w+') for i in range(2)]
+	tempFiles[0].write("OFF\n")
+
+	nodeIndices = {}
+	nodes = reader.readNode()
+	nodeCounter = 0
+	try:
+		while True:
+			n = nodes.next()
+			nodeCounter += 1
+			nodeIndices[n.label] = str(nodeCounter)
+			tempFiles[1].write(n.x+" "+n.y+" "+n.z+"\n")
+	except StopIteration:
+		pass
+
+	tempFiles[0].write(str(nodeCounter)+" ")
+
+	elements = reader.readElementIndexed()
+	elementCounter = 0
+	try:
+		while True:
+			e = elements.next()
+			tempFiles[1].write(str(len(e.list))+" "+" ".join([nodeIndices[i] for i in e.list])+"\n")
+			elementCounter += 1
+	except StopIteration:
+		pass
+
+	tempFiles[0].write(str(elementCounter)+" 0\n")
+
+	for f in tempFiles:
+		f.seek(0)
+		while True:
+			line = f.readline()
+			if line == "":
+				break
+			out.write(line)
+		f.close
+
+	if not string:
+		out.close()
 
