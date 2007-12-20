8d81c70e583c1d0ad0da437ba4433f057557ec83
bouzim <bouzim@1292ab78-9140-0410-bef9-b961eb31b77f>
2007-12-20 21:23:48 +0000

    * scripts/meshconvert   src/meshconvert/__init__.py: Move scripts/meshconvert content into __init__py; this way, meshconvert module can be imported and run in interactive sessions.

--- src/meshconvert/__init__.py
+++ src/meshconvert/__init__.py
@@ -1,2 +1,56 @@
 #! /usr/bin/env python
 
+from optparse import OptionParser
+import sys
+
+formats = {
+	(".unv",): "unv",
+	(".mesh", ".msh"): "mesh",
+	(".grd",): "gridgen",
+	(".vtp",): "vtk",
+	(".ply",): "ply",
+	(".off",): "off",
+	(".stl",): "stl",
+	(".obj",): "obj",
+}
+
+def getFormat(file, flag):
+	try:
+		suffix = file[file.rindex("."):].lower()
+		for list, format in formats.iteritems():
+			if suffix in list:
+				return format
+		raise ValueError
+	except ValueError:
+		raise ValueError, "Cannot guess file format for "+file+", please specify one with "+flag+" flag"
+
+def cli(argv, prog=None):
+	inputFormat = None
+	outputFormat = None
+	if prog is None:
+		prog = "meshconvert"
+	usage = "usage: %prog [options] INPUTFILE OUTPUTFILE"
+	parser = OptionParser(usage=usage, prog=prog)
+	parser.add_option("-I", "--input-format", metavar="STRING", action="store", type="string", dest="inputFormat", help="set input format")
+	parser.add_option("-O", "--output-format", metavar="STRING", action="store", type="string", dest="outputFormat", help="set output format")
+	(options, args) = parser.parse_args(args=argv)
+	if (len(args) != 2):
+		parser.error("incorrect number of arguments")
+	inputFile, outputFile = args
+
+	inputFormat = options.inputFormat and options.inputFormat or getFormat(inputFile, "-I")
+	outputFormat = options.outputFormat and options.outputFormat or getFormat(outputFile, "-O")
+
+	try:
+		inputModule =  __import__("meshconvert."+inputFormat, globals(),  locals(), [""])
+	except ImportError:
+		raise ValueError, "Unknown mesh format: "+inputFormat
+	try:
+		outputModule =  __import__("meshconvert."+outputFormat, globals(),  locals(), [""])
+	except ImportError:
+		raise ValueError, "Unknown mesh format: "+outputFormat
+	outputModule.writer(outputFile, inputModule.reader(inputFile))
+
+if __name__ == "__main__":
+	cli(sys.argv[1:], prog=sys.argv[0])
+
